f05851c5d68bf41b4ebdde175ae8d3025b0fc8b4
nerik <erik.escoffier@gmail.com>
2016-04-28 10:39:35 +0200

    added some comments

--- index.js
+++ index.js
@@ -14,7 +14,13 @@
       // you can get the native map to work with it
       var map = vis.getNativeMap();
 
+      // check out the actual viz.json file!
+      //layers[0]: positron basemap
+      //layers[1]: data layers
+      //layers[2]: positron basemap (labels)
       var datavizLayers = layers[1];
+
+      // in the data layer, we have 2 sublayers:
       var routes = window.routes = datavizLayers.getSubLayer(0);
       var stations = datavizLayers.getSubLayer(1);
       // console.log(stations)
@@ -24,12 +30,15 @@
       var routesCSS = window.routesCSS = '#osm_subway_routes_geojson{line-width: 10; line-opacity: 1;}';
       // routes.setCartoCSS(routesCSS);
 
-      // var sql = new cartodb.SQL({user: 'nerikcarto'});
+      // a standalone SQL client is embedded into cartodb.js, allowing us to perform queries 'outside of the map'
       var sql = new cartodb.SQL({user: 'erik-webinar'});
+
+      // fetch unique metro line colors in the dataset
       sql.execute("SELECT      distinct on(subway_color)      substring((relations::json->0->'reltags'->'colour')::text from 3 for 6) as subway_color      FROM osm_subway_routes_geojson")
         .done(function(colorsData) {
           // console.log(colorsData);
           colorsData.rows.forEach(function(row) {
+            // apply each color using a selector with the actual color value
             window.routesCSS += "#osm_subway_routes_geojson[subway_color='"+row.subway_color+"'] {line-color: #"+ row.subway_color +";}";
           })
           window.routes.setCartoCSS(window.routesCSS);
@@ -40,23 +49,29 @@
       // stations.setSQL("SELECT * FROM osm_subway_stations WHERE type_ratp = 'metro'")
       // stations.setSQL("SELECT * FROM osm_subway_stations WHERE type_ratp = 'metro' AND wheelchair = 'no'")
 
+      //grab that SQL from the HTML file
       var sqlTemplate = document.getElementById('js-stations-template').innerHTML;
 
+      // turn it into an Underscore template so that we can inject variables
       var compiledSQLTemplate = cartodb._.template(sqlTemplate);
 
       // var finalSQL = compiledSQLTemplate({wheelchair: 'yes'});
 
+      // ...which also allows us to use conditionals, for example to toggle between 2 visualisations
       var finalSQL = compiledSQLTemplate({showTraffic: true});
       stations.setSQL(finalSQL);
 
       console.log(finalSQL);
       stations.setSQL(finalSQL);
 
+      // use that computed circle_size column to show # of passengers per year on each staion circle
       stations.setCartoCSS('#osm_subway_stations{marker-fill:white; marker-width: [circle_size]}');
 
+      // mark the sublayer as interactive, then select which column values are returned in callbacks
       stations.setInteraction(true)
       stations.setInteractivity('cartodb_id,name,trafic')
 
+      // add listeners, see http://docs.cartodb.com/cartodb-platform/cartodb-js/events/
       stations.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
         console.log( data);
         document.getElementById('info').innerHTML = 'name: ' + data.name + '<br>traffic (2014): '+ data.trafic;
